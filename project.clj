a8eb33599c71fab2859ab9c0be8ebd1089cf049b
Artem Yankov <artem@badgeville.com>
2012-08-26 12:03:24 -0700

    initial structure

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject memobot "1.0.0-SNAPSHOT"
+  :description "Simple data structure server"
+  :dependencies [[org.clojure/clojure "1.4.0"]])
---
+++ test/memobot/test/core.clj
@@ -0,0 +1,6 @@
+(ns memobot.test.core
+  (:use [memobot.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
