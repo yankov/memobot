1cb0235f6716e2e3f206129cd66c3d1c2241887b
Artem Yankov <artem@badgeville.com>
2012-09-04 20:18:06 -0700

    implement sdiff

--- src/memobot/sets.clj
+++ src/memobot/sets.clj
@@ -1,8 +1,8 @@
 (ns memobot.sets
-  (:use [memobot types]))
+  (:use [memobot types]
+        [clojure.set]))
 
 ;TODO
-; scard
 ; sdiff
 ; sinter
 ; sismember
@@ -37,3 +37,15 @@
     (let [ck (get-atom db k)]
       [:int (count @ck)])
     [:czero]))
+
+;TODO: add support for more then 2 sets
+(defn sdiff-cmd
+  "Subtract multiple sets"
+  [db k1 k2]
+  (if (and (exists? db k1) (exists? db k2))
+    (let [s1 (get-atom db k1)
+          s2 (get-atom db k2)]
+       (if (and (set? @s1) (set? @s2))
+         [:ok (difference @s1 @s2)]
+         [:wrongtypeerr]))
+    [:emptymultibulk]))
