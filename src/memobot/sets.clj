2912ab51d5a1936b07bbb7cdaa3a73371a3fbdba
Artem Yankov <artem@badgeville.com>
2012-09-16 11:10:34 -0700

    refactor sets

--- src/memobot/sets.clj
+++ src/memobot/sets.clj
@@ -2,97 +2,52 @@
   (:use [memobot types]
         [clojure.set]))
 
-;TODO: add support for operations with >2 sets
-
 (defn sadd-cmd
   "Add one or more members to a set"
-  [db k v]
-  (if (not (exists? db k))
-    (do
-      (intern db (symbol k) (atom (conj #{} (fix-type v))))
-      [:cone])
-    (let [ck (get-atom db k)
-          value (fix-type v)]
-      (if (set? @ck) 
-        (if (not (contains? @ck value))
-          (do
-            (swap! ck #(conj % value))
-            [:cone])
-          [:czero])
-        [:wrongtypeerr]))))
+  [k v]
+  (let [s (eval k)]
+    (if (not (contains? @s v))
+      (do
+        (swap! s #(conj % v))
+        1)
+      0)))
 
 (defn scard-cmd
   "Get the number of members in a set"
-  [db k]
-  (if (exists? db k)
-    (let [ck (get-atom db k)]
-      [:int (count @ck)])
-    [:czero]))
+  [k]
+  (count k))
 
 (defn sdiff-cmd
   "Subtract multiple sets"
-  [db k1 k2]
-  (if (and (exists? db k1) (exists? db k2))
-    (let [s1 (get-atom db k1)
-          s2 (get-atom db k2)]
-       (if (and (set? @s1) (set? @s2))
-         [:ok (difference @s1 @s2)]
-         [:wrongtypeerr]))
-    [:emptymultibulk]))
+  [k1 k2]
+  (difference k1 k2))
 
 (defn sinter-cmd
   "Intersect multiple sets"
-  [db k1 k2]
-  (if (and (exists? db k1) (exists? db k2))
-    (let [s1 (get-atom db k1)
-          s2 (get-atom db k2)]
-       (if (and (set? @s1) (set? @s2))
-         [:ok (intersection @s1 @s2)]
-         [:wrongtypeerr]))
-    [:emptymultibulk]))
+  [k1 k2]
+  (intersection k1 k2))
 
 (defn sismember-cmd
   "Determine if a given value is a member of a set"
-  [db k v]
-  (if (exists? db k)
-    (let [s (get-atom db k)
-          value (fix-type v)]
-      [:int (if (contains? @s value) 1 0)])
-    [:czero]))
+  [k v]
+  (if (contains? k v) 1 0))
 
 (defn smembers-cmd
   "Get all the members in a set"
-  [db k]
-  (if (exists? db k)
-    (let [s (get-atom db k)]
-      (if (set? @s)
-        [:ok @s]
-        [:wrongtypeerr]))
-    [:emptymultibulk]))    
+  [k]
+  k)
 
 (defn srem-cmd
   "Remove one or more members from a set"
-  [db k v]
-  (if (exists? db k)
-    (let [s (get-atom db k)
-          value (fix-type v)]
-      (if (contains? @s value) 
-        (do 
-          (swap! s #(disj % value))
-          [:cone])
-        [:czero]))
-    [:czero]))
+  [k v]
+  (let [s (eval k)]
+    (if (contains? @s v)
+      (do
+        (swap! s #(disj % v))
+        1)
+      0)))
 
 (defn sunion-cmd
   "Add multiple sets"
-  [db k1 k2]
-  (if (and (exists? db k1) (exists? db k2))
-    (let [s1 (get-atom db k1)
-          s2 (get-atom db k2)]
-       (if (and (set? @s1) (set? @s2))
-         [:ok (union @s1 @s2)]
-         [:wrongtypeerr]))
-    [:emptymultibulk]))
-
-
-  
+  [k1 k2]
+  (union k1 k2))
